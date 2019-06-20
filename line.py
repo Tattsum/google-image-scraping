a0e5a749d355f186f29bef3c20a7fef1cab14b6d
Tattsum <kurohari35@gmail.com>
2019-06-20 16:10:38 +0900

    scraping結果をlineに通知

--- line.py
+++ line.py
@@ -1,15 +1,36 @@
 #-*- coding: utf-8-*-
 
 import requests
+import datetime
+import googleimagescraping
+import argparse
+import os
 
-def line():
+
+def line(message, images):
     url = "https://notify-api.line.me/api/notify"
-    token = "発行されたトークンをここに記入"
+    token = "vsX9w5PycFVrnzvgJzbvYyHYrFcMYd9yUTPEIpCZy6H"
     headers = {"Authorization": "Bearer " + token}
-    message = 'テスト'
     payload = {"message":  message}
-    requests.post(url, headers=headers, params=payload)
 
+    if len(images) == 0:
+        # 画像なしテキストのみ送信
+        requests.post(url, data=payload, headers=headers)
+    else:
+        # 画像ありテキスト＋画像送信
+        files = {"imageFile": open(images, "rb")}
+        requests.post(url, data=payload, headers=headers, files=files)
+
+def main():
+    googleimagescraping.scraping()
+    message = '嵐 最高!!'
+    path = "./img/櫻井翔"
+    base_path = os.path.abspath("./img/櫻井翔")
+    for filename in os.listdir(base_path):
+        if os.path.isfile(os.path.join(base_path, filename)):
+            files = path + "/" + filename
+            file_full_path = os.path.abspath(files)
+            line(message,file_full_path)
 
 if __name__ == '__main__':
-    line()
+    main()
